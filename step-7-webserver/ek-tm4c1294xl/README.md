b0dcd27e610739e93edd4e35a4a04be49955e85a
Sergio R. Caprile <scaprile@gmail.com>
2022-12-13 11:40:32 -0300

    Add TI board
    Added a brief README with a link to the Uniflash part in the baremetal tutorial

---
+++ step-7-webserver/ek-tm4c1294xl/README.md
@@ -0,0 +1,4 @@
+## Additional Requirements
+
+- [Uniflash](https://mongoose.ws/tutorials/ti/ek-tm4c1294xl-baremetal/#build-and-run) for flashing
+
---
+++ step-7-webserver/ek-tm4c1294xl/include/TM4C129.h
@@ -0,0 +1,61 @@
+/**************************************************************************//**
+ * @file     TM4C129.h
+ * @brief    CMSIS Device System Header File for
+ *           Texas Instruments Tiva TM4C129 Device Series
+ * @version  V12591
+ * @date     12. February 2014
+ *
+ * @note
+ *
+ ******************************************************************************/
+
+#ifndef TM4C129_H
+#define TM4C129_H
+
+#if defined(SC4CE290NCPZ)
+  #include "SC4CE290NCPZ.h"
+#elif defined(TM4C1290NCPDT)
+  #include "TM4C1290NCPDT.h"
+#elif defined(TM4C1290NCZAD)
+  #include "TM4C1290NCZAD.h"
+#elif defined(TM4C1292NCPDT)
+  #include "TM4C1292NCPDT.h"
+#elif defined(TM4C1292NCZAD)
+  #include "TM4C1292NCZAD.h"
+#elif defined(TM4C1294KCPDT)
+  #include "TM4C1294KCPDT.h"
+#elif defined(TM4C1294NCPDT)
+  #include "TM4C1294NCPDT.h"
+#elif defined(TM4C1294NCZAD)
+  #include "TM4C1294NCZAD.h"
+#elif defined(TM4C1297NCZAD)
+  #include "TM4C1297NCZAD.h"
+#elif defined(TM4C1299KCZAD)
+  #include "TM4C1299KCZAD.h"
+#elif defined(TM4C1299NCZAD)
+  #include "TM4C1299NCZAD.h"
+#elif defined(TM4C129CNCPDT)
+  #include "TM4C129CNCPDT.h"
+#elif defined(TM4C129CNCZAD)
+  #include "TM4C129CNCZAD.h"
+#elif defined(TM4C129DNCPDT)
+  #include "TM4C129DNCPDT.h"
+#elif defined(TM4C129DNCZAD)
+  #include "TM4C129DNCZAD.h"
+#elif defined(TM4C129EKCPDT)
+  #include "TM4C129EKCPDT.h"
+#elif defined(TM4C129ENCPDT)
+  #include "TM4C129ENCPDT.h"
+#elif defined(TM4C129ENCZAD)
+  #include "TM4C129ENCZAD.h"
+#elif defined(TM4C129LNCZAD)
+  #include "TM4C129LNCZAD.h"
+#elif defined(TM4C129XKCZAD)
+  #include "TM4C129XKCZAD.h"
+#elif defined(TM4C129XNCZAD)
+  #include "TM4C129XNCZAD.h"
+#else
+#error "TM4C129.h: TM4C129 Device NOT specified"
+#endif
+
+#endif /* TM4C129_H */
