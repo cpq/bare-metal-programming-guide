bee7ab274b1c47326419c840480780c552c2fbc0
Maksym Shutiak <shutyak.maxym@gmail.com>
2024-09-05 14:10:07 +0300

    bugfix: naming in variables for builtin buttons and leds

--- templates/blinky/nRF52840dk/main.c
+++ templates/blinky/nRF52840dk/main.c
@@ -34,7 +34,7 @@ __attribute__((naked, noreturn)) void _reset(void) {
 
 extern void _estack(void);  // Defined in link.ld
 
-// 16 standard and 91 STM32-specific handlers
+// 16 standard and 42 nRF-specific handlers
 __attribute__((section(".vectors"))) void (*const tab[16 + 42])(void) = {
     _estack, _reset};
 
